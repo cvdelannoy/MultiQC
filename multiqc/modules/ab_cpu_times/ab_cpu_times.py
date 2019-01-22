c14f63c1d8a98f474b3b23434d34b4b56c42f1b8
Carlos de Lannoy <cvdelannoy@gmail.com>
2019-01-22 11:59:26 +0100

    order entries in cpu performance table

--- multiqc/modules/ab_cpu_times/ab_cpu_times.py
+++ multiqc/modules/ab_cpu_times/ab_cpu_times.py
@@ -2,6 +2,7 @@ from multiqc.modules.base_module import BaseMultiqcModule
 from multiqc.plots import table
 import logging
 import csv
+from collections import OrderedDict
 
 # Initialise the logger
 log = logging.getLogger(__name__)
@@ -53,23 +54,35 @@ class MultiqcModule(BaseMultiqcModule):
         cpu_dict_gs = dict()
         for fc in list_f:
             cpu_list = list(csv.reader(fc['f'].split('\n'), delimiter='\t'))
-            cpu_dict_cur = dict()
+            cpu_dict_cur = OrderedDict()
+            cpu_dict_order = dict()
             cpu_dict_gs_cur = dict()
             for k, v in zip(cpu_list[0], cpu_list[1]):
                 if k == 'h:m:s':
                     cpu_dict_cur['Wall time'] = v
                     cpu_dict_gs_cur['Wall time'] = v
+                    cpu_dict_order[1] = 'Wall time'
                 elif k == 'max_pss':
                     cpu_dict_cur['peak PSS (MB)'] = float(v)
                     cpu_dict_gs_cur['peak CPU usage (PSS) (MB)'] = float(v)
+                    cpu_dict_order[2] = 'peak PSS (MB)'
                 elif k == 'max_uss':
                     cpu_dict_cur['peak USS (MB)'] = float(v)
+                    cpu_dict_order[3] = 'peak USS (MB)'
                 elif k == 'mean_load':
                     cpu_dict_cur['mean CPU load (MB)'] = float(v)
+                    cpu_dict_order[4] = 'mean CPU load (MB)'
                 elif k == 'io_in':
                     cpu_dict_cur['I/O in (MB/s)'] = float(v)
+                    cpu_dict_order[5] = 'I/O in (MB/s)'
                 elif k == 'io_out':
                     cpu_dict_cur['I/O out (MB/s)'] = float(v)
+                    cpu_dict_order[6] = 'I/O out (MB/s)'
+            # enforce order in dict
+            cpu_list_order = list(cpu_dict_order)
+            cpu_list_order.sort()
+            for n in cpu_list_order:
+                cpu_dict_cur.move_to_end(cpu_dict_order[n])
             cpu_dict[fc['s_name']] = cpu_dict_cur
             cpu_dict_gs[fc['s_name']] = cpu_dict_gs_cur
         self._cpu_usage = cpu_dict
